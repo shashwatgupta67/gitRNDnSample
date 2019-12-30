b8b853fb606eca39b5c897236551667013febfac
Shashwat Gupta <shaswat@mapmyindia.com>
2019-12-30 11:38:12 +0530

    Sample pdf, library of itext and associate bean for genrate pdf is added. Some change appear in token match code.

---
+++ Bean.java
@@ -0,0 +1,478 @@
+package main.java;
+
+public class Bean {
+	
+    private String bookingNumber;
+    private String vehicleName;
+    private String pickUpLocation;
+    private String dropLocation;
+    private String journeyDate;
+    private String passengers;
+    private String luggage;
+
+    private String fullName;
+    private String fname;
+    private String lname;
+    private String gender;
+    private String email;
+    private String pickUpSign;
+    private String msgToDriver;
+    private String countryCode;
+    private String phoneNo;
+    private String fullPhoneNo;
+    private String totalBill;
+
+    private String img_trip;
+    private String img_note;
+    private String img_Detail;
+    private String img_logo;
+    
+    private String tripType;
+    private String flightNo;
+    private String promoCode;
+    private String emailSignature;
+   
+    private String hourlyText;
+    private String startTime;
+    private String serviceType;
+    private String rentalType;
+    private String meetingPoint;
+
+    /**
+     * @return the bookingNumber
+     */
+    public String getBookingNumber() {
+        return bookingNumber;
+    }
+
+    /**
+     * @param bookingNumber the bookingNumber to set
+     */
+    public void setBookingNumber(String bookingNumber) {
+        this.bookingNumber = bookingNumber;
+    }
+
+    /**
+     * @return the vehicleName
+     */
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    /**
+     * @param vehicleName the vehicleName to set
+     */
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    /**
+     * @return the pickUpLocation
+     */
+    public String getPickUpLocation() {
+        return pickUpLocation;
+    }
+
+    /**
+     * @param pickUpLocation the pickUpLocation to set
+     */
+    public void setPickUpLocation(String pickUpLocation) {
+        this.pickUpLocation = pickUpLocation;
+    }
+
+    /**
+     * @return the dropLocation
+     */
+    public String getDropLocation() {
+        return dropLocation;
+    }
+
+    /**
+     * @param dropLocation the dropLocation to set
+     */
+    public void setDropLocation(String dropLocation) {
+        this.dropLocation = dropLocation;
+    }
+
+    /**
+     * @return the journeyDate
+     */
+    public String getJourneyDate() {
+        return journeyDate;
+    }
+
+    /**
+     * @param journeyDate the journeyDate to set
+     */
+    public void setJourneyDate(String journeyDate) {
+        this.journeyDate = journeyDate;
+    }
+
+    /**
+     * @return the passengers
+     */
+    public String getPassengers() {
+        return passengers;
+    }
+
+    /**
+     * @param passengers the passengers to set
+     */
+    public void setPassengers(String passengers) {
+        this.passengers = passengers;
+    }
+
+    /**
+     * @return the luggage
+     */
+    public String getLuggage() {
+        return luggage;
+    }
+
+    /**
+     * @param luggage the luggage to set
+     */
+    public void setLuggage(String luggage) {
+        this.luggage = luggage;
+    }
+
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the fname
+     */
+    public String getFname() {
+        return fname;
+    }
+
+    /**
+     * @param fname the fname to set
+     */
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    /**
+     * @return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the pickUpSign
+     */
+    public String getPickUpSign() {
+        return pickUpSign;
+    }
+
+    /**
+     * @param pickUpSign the pickUpSign to set
+     */
+    public void setPickUpSign(String pickUpSign) {
+        this.pickUpSign = pickUpSign;
+    }
+
+    /**
+     * @return the msgToDriver
+     */
+    public String getMsgToDriver() {
+        return msgToDriver;
+    }
+
+    /**
+     * @param msgToDriver the msgToDriver to set
+     */
+    public void setMsgToDriver(String msgToDriver) {
+        this.msgToDriver = msgToDriver;
+    }
+
+    /**
+     * @return the countryCode
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * @param countryCode the countryCode to set
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    /**
+     * @return the phoneNo
+     */
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    /**
+     * @param phoneNo the phoneNo to set
+     */
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    /**
+     * @return the fullPhoneNo
+     */
+    public String getFullPhoneNo() {
+        return fullPhoneNo;
+    }
+
+    /**
+     * @param fullPhoneNo the fullPhoneNo to set
+     */
+    public void setFullPhoneNo(String fullPhoneNo) {
+        this.fullPhoneNo = fullPhoneNo;
+    }
+
+    /**
+     * @return the totalBill
+     */
+    public String getTotalBill() {
+        return totalBill;
+    }
+
+    /**
+     * @param totalBill the totalBill to set
+     */
+    public void setTotalBill(String totalBill) {
+        this.totalBill = totalBill;
+    }
+
+    /**
+     * @return the img_trip
+     */
+    public String getImg_trip() {
+        return img_trip;
+    }
+
+    /**
+     * @param img_trip the img_trip to set
+     */
+    public void setImg_trip(String img_trip) {
+        this.img_trip = img_trip;
+    }
+
+    /**
+     * @return the img_note
+     */
+    public String getImg_note() {
+        return img_note;
+    }
+
+    /**
+     * @param img_note the img_note to set
+     */
+    public void setImg_note(String img_note) {
+        this.img_note = img_note;
+    }
+
+    /**
+     * @return the img_Detail
+     */
+    public String getImg_Detail() {
+        return img_Detail;
+    }
+
+    /**
+     * @param img_Detail the img_Detail to set
+     */
+    public void setImg_Detail(String img_Detail) {
+        this.img_Detail = img_Detail;
+    }
+
+    /**
+     * @return the img_logo
+     */
+    public String getImg_logo() {
+        return img_logo;
+    }
+
+    /**
+     * @param img_logo the img_logo to set
+     */
+    public void setImg_logo(String img_logo) {
+        this.img_logo = img_logo;
+    }
+
+    /**
+     * @return the tripType
+     */
+    public String getTripType() {
+        return tripType;
+    }
+
+    /**
+     * @param tripType the tripType to set
+     */
+    public void setTripType(String tripType) {
+        this.tripType = tripType;
+    }
+
+    /**
+     * @return the flightNo
+     */
+    public String getFlightNo() {
+        return flightNo;
+    }
+
+    /**
+     * @param flightNo the flightNo to set
+     */
+    public void setFlightNo(String flightNo) {
+        this.flightNo = flightNo;
+    }
+
+    /**
+     * @return the promoCode
+     */
+    public String getPromoCode() {
+        return promoCode;
+    }
+
+    /**
+     * @param promoCode the promoCode to set
+     */
+    public void setPromoCode(String promoCode) {
+        this.promoCode = promoCode;
+    }
+
+    /**
+     * @return the emailSignature
+     */
+    public String getEmailSignature() {
+        return emailSignature;
+    }
+
+    /**
+     * @param emailSignature the emailSignature to set
+     */
+    public void setEmailSignature(String emailSignature) {
+        this.emailSignature = emailSignature;
+    }
+
+    /**
+     * @return the hourlyText
+     */
+    public String getHourlyText() {
+        return hourlyText;
+    }
+
+    /**
+     * @param hourlyText the hourlyText to set
+     */
+    public void setHourlyText(String hourlyText) {
+        this.hourlyText = hourlyText;
+    }
+
+    /**
+     * @return the startTime
+     */
+    public String getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * @param startTime the startTime to set
+     */
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * @return the serviceType
+     */
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    /**
+     * @param serviceType the serviceType to set
+     */
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the rentalType
+     */
+    public String getRentalType() {
+        return rentalType;
+    }
+
+    /**
+     * @param rentalType the rentalType to set
+     */
+    public void setRentalType(String rentalType) {
+        this.rentalType = rentalType;
+    }
+
+    /**
+     * @return the meetingPoint
+     */
+    public String getMeetingPoint() {
+        return meetingPoint;
+    }
+
+    /**
+     * @param meetingPoint the meetingPoint… to set
+     */
+    public void setMeetingPoint(String meetingPoint) {
+        this.meetingPoint = meetingPoint;
+    }
+    
+    
+
+
+}
